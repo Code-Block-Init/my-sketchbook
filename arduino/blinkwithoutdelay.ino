d48034062dbce90d2bdb080952623af47ce6a032
Aswini S <ashumeow@live.com>
2015-08-12 11:28:03 +0530

    Update blinkwithoutdelay.ino

--- arduino/blinkwithoutdelay.ino
+++ arduino/blinkwithoutdelay.ino
@@ -31,16 +31,14 @@ void setup() {
 
 void loop()
 {
-  // here is where you'd put code that needs to be running all the time.
-
   // check to see if it's time to blink the LED; that is, if the
-  // difference between the current time and last time you blinked
+  // difference between the current time and last time that blinked
   // the LED is bigger than the interval at which you want to
   // blink the LED.
   unsigned long currentMillis = millis();
  
   if(currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED 
+    // save the last time that blinked the LED 
     previousMillis = currentMillis;   
 
     // if the LED is off turn it on and vice-versa:
